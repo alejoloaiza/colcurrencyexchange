b2290862fca5650e5996acf793f57a90f3a5568a
atomixxx <alejo.loaiza@gmail.com>
2018-04-11 09:02:05 -0500

    [Add] Added another exchange

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-
 	"regexp"
 	"strings"
 
@@ -22,13 +21,13 @@ func main() {
 	cNuti := colly.NewCollector(
 		colly.CacheDir("./cache"),
 	)
-	var Titles []string
-	var Prices []string
+	var Titles1 []string
+	var Prices1 []string
 
 	cNutiurlToVisit := "http://nutifinanzas.com/"
 	cNuti.OnHTML("div.col-md-12.TituloDivisa", func(e *colly.HTMLElement) {
 		//fmt.Println(strings.TrimSpace(e.Text))
-		Titles = append(Titles, strings.TrimSpace(e.Text))
+		Titles1 = append(Titles1, strings.TrimSpace(e.Text))
 	})
 	cNuti.OnHTML("div.col-md-9", func(e *colly.HTMLElement) {
 		re := regexp.MustCompile(`\r?\n`)
@@ -37,7 +36,7 @@ func main() {
 		TempPrice = strings.Replace(TempPrice, " ", "", -1)
 		TempPrice = strings.Replace(TempPrice, "Compramos:$", "", -1)
 		TempPrice = strings.Replace(TempPrice, "Vendemos:$", "@", -1)
-		Prices = append(Prices, TempPrice)
+		Prices1 = append(Prices1, TempPrice)
 	})
 	cNuti.OnResponse(func(r *colly.Response) {
 		//fmt.Printf(string(r.Body))
@@ -47,14 +46,14 @@ func main() {
 		//	fmt.Println(r.Headers)
 	})
 	cNuti.OnScraped(func(r *colly.Response) {
-		for i, curval := range Titles {
+		for i, curval := range Titles1 {
 			var extData ExchangeData
-			extprice := strings.Split(Prices[i], "@")
+			extprice := strings.Split(Prices1[i], "@")
 			extData.Name = "Nutifinanzas"
 			extData.Currency = curval
 			extData.PriceBuy = extprice[0]
 			extData.PriceSell = extprice[1]
-			fmt.Printf("%s: C: %s V: %s\r\n", extData.Currency, extData.PriceBuy, extData.PriceSell)
+			fmt.Printf("%s > %s > C: %s V: %s\r\n", extData.Name, extData.Currency, extData.PriceBuy, extData.PriceSell)
 			generalData = append(generalData, extData)
 		}
 	})
@@ -63,5 +62,62 @@ func main() {
 	})
 	cNuti.Visit(cNutiurlToVisit)
 	cNuti.Wait()
+
+	var Titles2 []string
+	var Prices2 []string
+	var Prices3 []string
+	cUni := colly.NewCollector(
+	//colly.CacheDir("./cache"),
+	)
+
+	cUniurlToVisit := "http://www.unicambios.com.co/"
+	cUni.OnHTML("[class^=row_][class$=col_1]", func(e *colly.HTMLElement) {
+		if strings.TrimSpace(e.Text) != "" {
+			Titles2 = append(Titles2, strings.TrimSpace(e.Text))
+		}
+	})
+	cUni.OnHTML("[class^=row_][class$=col_2]", func(e *colly.HTMLElement) {
+		//fmt.Println(e.Attr("class"))
+		if strings.Contains(e.Attr("class"), "row_0 ") || strings.Contains(e.Attr("class"), "row_1 ") {
+			return
+		} else {
+			if strings.TrimSpace(e.Text) != "" {
+				Prices2 = append(Prices2, strings.TrimSpace(e.Text))
+			}
+		}
+	})
+	cUni.OnHTML("[class^=row_][class$=col_3]", func(e *colly.HTMLElement) {
+		if strings.Contains(e.Attr("class"), "row_0 ") || strings.Contains(e.Attr("class"), "row_1 ") {
+			return
+		} else {
+			if strings.TrimSpace(e.Text) != "" {
+				Prices3 = append(Prices3, strings.TrimSpace(e.Text))
+			}
+		}
+
+	})
+	cUni.OnResponse(func(r *colly.Response) {
+		//fmt.Printf(string(r.Body))
+	})
+	cUni.OnRequest(func(r *colly.Request) {
+		//r.Ctx.Put("url", r.URL.String())
+		//	fmt.Println(r.Headers)
+	})
+	cUni.OnScraped(func(r *colly.Response) {
+		for i, curval := range Titles2 {
+			var extData ExchangeData
+			extData.Name = "Unicambios"
+			extData.Currency = curval
+			extData.PriceBuy = Prices2[i]
+			extData.PriceSell = Prices3[i]
+			fmt.Printf("%s > %s > C: %s V: %s\r\n", extData.Name, extData.Currency, extData.PriceBuy, extData.PriceSell)
+			generalData = append(generalData, extData)
+		}
+	})
+	cUni.OnError(func(r *colly.Response, err error) {
+		fmt.Println("DETAILS: Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
+	cUni.Visit(cUniurlToVisit)
+	cUni.Wait()
 
 }
